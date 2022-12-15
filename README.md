1c4abd509dad1cb4d1f3d47f5049a7090c79b333
Ramsin Khoshabeh <ramsin102@gmail.com>
2022-12-15 15:19:36 -0800

    add source files

---
+++ README.md
@@ -0,0 +1,32 @@
+# FastAPI Starter
+
+## Prerequesites
+
+You have Python 3.7+ installed
+
+1. Create a Python virtual environment
+
+    ```bash
+    python3 -m venv env
+    ```
+
+2. Run the server
+
+    ```bash
+    uvicorn server:router --reload
+    ```
+
+3. Visit the server in your browser with these 4 example routes:
+
+    * GET an HTML page: `http://127.0.0.1:8000`
+    * GET a templatized HTML partial: `http://127.0.0.1:8000/html/7`
+    * GET a JSON response: `http://127.0.0.1:8000/json/steve`
+    * GET a JSON response with query arguments: `http://127.0.0.1:8000/items/2?q=hello`
+
+4. View the automatically generated docs for the routes!
+
+    `http://127.0.0.1:8000/docs`
+
+    or an alternative UI...
+
+    `http://127.0.0.1:8000/redoc`
---
+++ server.py
@@ -0,0 +1,42 @@
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# Necessary Imports
+from fastapi import FastAPI, Request            # The main FastAPI import and Request object
+from fastapi.responses import HTMLResponse      # Used for returning HTML responses (JSON is default)
+from fastapi.templating import Jinja2Templates  # Used for generating HTML from templatized files
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# Configuration
+router = FastAPI()                              # Specify the "app" that will run the routing
+views = Jinja2Templates(directory="views")      # Specify where the HTML files are located on disk
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# Example route: return a static HTML page
+@router.get("/", response_class=HTMLResponse)
+def get_html() -> HTMLResponse:
+  with open("views/hello.html") as html:
+    return HTMLResponse(content=html.read())
+
+# Example route: returning an HTML partial using Jinja2 template rendering
+@router.get("/html/{id}", response_class=HTMLResponse)
+def get_html_partial(request:Request, id:str) -> HTMLResponse:
+  return views.TemplateResponse("hello_partial.html", {"request":request, "id":id})
+
+# Example route: returning just JSON
+@router.get("/json/{name}")
+def get_json(name:str) -> dict:
+  return {"Hello": name}
+
+# Example route: returning JSON with an additional query argument
+@router.get("/items/{item_id}")
+def read_item(item_id:int, q:str|None=None):
+  return {"item_id": item_id, "query_string": q}
+
+# # AsyncIO Version
+
+# @router.get("/")
+# async def read_root():
+#     return {"Hello": "World"}
+
+# @router.get("/items/{item_id}")
+# async def read_item(item_id: int, q: str|None=None):
+#     return {"item_id": item_id, "query_string": q}
